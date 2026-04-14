0c813a345ad5ccb9a6616f08db8b84905f012ed5
Sanket H Fere <154800998+sanketfere@users.noreply.github.com>
2026-04-15 00:42:14 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello from GitHub!");
         System.out.println("Auto build triggered!");
         System.out.println("Web-Hook Added");
+        System.out.println("Created a Jenkins declarative pipeline that uses multiple triggers, such as GitHub push webhook and cron schedule.");
     }
 }
