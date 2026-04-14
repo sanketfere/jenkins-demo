3f0b3afb5e2ed685e73338ce35d20119967cbc5b
Sanket H Fere <154800998+sanketfere@users.noreply.github.com>
2026-04-14 23:14:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub!");
         System.out.println("Auto build triggered!");
+        System.out.println("Web-Hook Added");
     }
 }
